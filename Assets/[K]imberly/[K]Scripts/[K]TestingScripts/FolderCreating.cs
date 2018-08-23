fdc971c5a3235a81c65287ab82dca436be646fd4
Carelle <steamtik@gmail.com>
2018-08-22 20:24:27 -0700

    Added some testing scripts and logo

---
+++ Assets/[K]imberly/[K]Scripts/[K]TestingScripts/FolderCreating.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class PlayerInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Race { get; set; }
+}
+
+public class FolderCreating : MonoBehaviour
+{
+    string playerInfoSaving;
+    // Use this for initialization
+    void Start()
+    {
+        var folder = Directory.CreateDirectory("Dar");
+        Debug.Log(Directory.GetDirectories("Dar"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PlayerInfo player = new PlayerInfo()
+        {
+
+            Name = "No",
+            Age = 4,
+            Race = "Fuck you"
+        };
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerInfoSaving = JsonConvert.SerializeObject(player, Formatting.Indented);
+            using (StreamWriter sw = File.CreateText("Dar/testingDar.json"))
+            {
+                sw.Write(playerInfoSaving);
+            }
+        }
+    }
+}
