0dc6391f698e48b0a3ca21df7293fdd5e1be7665
QuestGiver <cpt.gastard@gmail.com>
2018-08-30 15:21:33 -0700

    add character controller and camera manager

---
+++ Assets/[P]aul/[P]Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float mouseSensativity = 300;
+    public CinemachineFreeLook freeLook;
+    // Use this for initialization
+    void Start()
+    {
+        if (freeLook == null)
+        {
+            Debug.LogError("No CharacterController assigned to Object: " + gameObject.name);
+        }
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.AngleAxis(freeLook.m_XAxis.Value,Vector3.up);
+    }
+}
---
+++ Assets/[P]aul/[P]Scripts/CharacterMotion.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotion : MonoBehaviour
+{
+    
+    public CharacterController controller;
+    //normalized direction
+
+    public float speed;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (controller == null)
+        {
+            Debug.LogError("No CharacterController assigned to Object: " + gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 normalDir = Vector3.zero;
+
+
+
+        if (Input.anyKey)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                normalDir += transform.forward;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                normalDir -= transform.right;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                normalDir -= transform.forward;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                normalDir += transform.right;
+            }
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                controller.Move(normalDir.normalized * (speed * 2) * Time.deltaTime);
+            }
+            else
+            {
+                controller.Move(normalDir.normalized * (speed * Time.deltaTime));
+            }
+        }
+    }
+}
