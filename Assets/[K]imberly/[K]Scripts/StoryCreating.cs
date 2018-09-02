c5967b3451c6dd48165626dd64c2ad646188f62e
Carelle <steamtik@gmail.com>
2018-09-02 08:25:23 -0700

    Made changes to the file saving

--- Assets/[K]imberly/[K]Scripts/StoryCreating.cs
+++ Assets/[K]imberly/[K]Scripts/StoryCreating.cs
@@ -10,23 +10,57 @@ public class StoryCreating : MonoBehaviour
     TMPro.TMP_Text erroeMessage;
     [SerializeField]
     TMPro.TMP_Text genreDropDown;
+    [SerializeField]
+    Toggle ChapterToggle;
+    [SerializeField]
+    Toggle shortToggle;
+
+    private bool chapterStory;
     private void Start()
     {
         erroeMessage.enabled = false;
+        ChapterToggle.isOn = false;
+    }
+
+    public void ChapterStoryToggle()
+    {
+       
+        chapterStory = true;
+    }
+
+    public void ShortStoryToggle()
+    {
+      
+        chapterStory = false;
     }
 
     public void CreateStory(TMPro.TMP_InputField storytitle)
     {
-        if (Directory.Exists("Story_Creation\\" + genreDropDown.text + "\\"  + storytitle.text))
+        if (Directory.Exists("Story_Creation\\Chapter_Stories\\" + genreDropDown.text + "\\"  + storytitle.text) && chapterStory == true)
         {
             erroeMessage.enabled = true;
             erroeMessage.text = storytitle.text + " Already Exists";
+            Debug.Log("ChapterSotry");
+        }
+        else if(Directory.Exists("Story_Creation\\Short_Stories\\" + genreDropDown.text + "\\" + storytitle.text) && chapterStory == false)
+        {
+            erroeMessage.enabled = true;
+            erroeMessage.text = storytitle.text + " Already Exists";
+            Debug.Log("ShortSotry");
         }
         else
         {
             erroeMessage.enabled = false;
-            Directory.CreateDirectory("Story_Creation\\" + genreDropDown.text + "\\" + storytitle.text);
+            if (chapterStory)
+            {
+                Directory.CreateDirectory("Story_Creation\\Chapter_Stories\\" + genreDropDown.text + "\\" + storytitle.text);
+            }
+            else
+            {
+                Directory.CreateDirectory("Story_Creation\\Short_Stories\\" + genreDropDown.text + "\\" + storytitle.text);
+            }
         }
+       
 
     }
 }
